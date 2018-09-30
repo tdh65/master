a3591945581270f32211cd41dfa63857005810f6
root <root@karapate>
2018-09-30 19:16:03 +0200

    nouveau mise_a_jour

--- mise_a_jour_System.sh
+++ mise_a_jour_System.sh
@@ -1,1 +1,1 @@
-apt update -y; apt upgrade -y ; apt autoremove -y ; apt autoremove -y ; chkrootkit
+su - -c "apt update -y"; su - -c " apt upgrade -y" ; su - -c "apt autoremove -y" ; su - -c "apt autoremove -y "; su - -c "chkrootkit" ; exit
